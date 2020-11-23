d202e9a010c358972672a32b7a45fd927479fd13
superman285 <superman285@126.com>
2020-11-23 20:13:31 +0800

    VBlog-CMS strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39b68a23896cffb10d8ba348b07cd2fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28dd064d-2cd2-4968-9e6a-b2b6e6541a23'
+};
